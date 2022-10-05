008584e58e2aecc8d37f31cb7e64d15b3f904d77
jorgerod <jorgeroma2000@gmail.com>
2022-10-05 16:59:07 +0200

    printf with comments

--- circle_1/printf_pr/printf/ft_printf.c
+++ circle_1/printf_pr/printf/ft_printf.c
@@ -6,13 +6,19 @@
 /*   By: jorgerod <jorgerod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 18:29:27 by jorgerod          #+#    #+#             */
-/*   Updated: 2022/10/05 15:31:11 by jorgerod         ###   ########.fr       */
+/*   Updated: 2022/10/05 16:37:52 by jorgerod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
 
+/**
+ * @brief Finds the number of characters necesary to represent a number.
+ * 
+ * @param n	Number.
+ * @return int	Number of characters necesary to represent n.
+ */
 int	ft_int_length(long long int n)
 {
 	int	i;
@@ -30,6 +36,13 @@ int	ft_int_length(long long int n)
 	return (i);
 }
 
+/**
+ * @brief Extension of ft_match_case.
+ * 
+ * @param ptr	Pointer to the variable argument list.
+ * @param c	Conversion identifier character.
+ * @param counter	Counter of characters printed by ft_printf.
+ */
 static void	ft_match_case_exted(va_list *ptr, char c, int *counter)
 {
 	long long int			n;
@@ -56,6 +69,14 @@ static void	ft_match_case_exted(va_list *ptr, char c, int *counter)
 	}
 }
 
+/**
+ * @brief Checks with character matchs with c and executes the corresponding
+ * 			function.
+ * 
+ * @param ptr	Pointer to the variable argument list.
+ * @param c	Conversion identifier character.
+ * @param counter	Counter of characters printed by ft_printf.
+ */
 static void	ft_match_case(va_list *ptr, char c, int *counter)
 {
 	if (c == 'c')
@@ -80,6 +101,14 @@ static void	ft_match_case(va_list *ptr, char c, int *counter)
 		ft_match_case_exted(ptr, c, counter);
 }
 
+/**
+ * @brief Writes an space if needed adn checks if the conversion parameter
+ * 			recognized, if so checks wich one with ft_match_case.
+ * 
+ * @param ptr	Pointer to the variable argument list.
+ * @param str	Pointer to the input string.
+ * @param counter	Counter of characters printed by ft_printf.
+ */
 static void	ft_match(va_list *ptr, char **str, int *counter)
 {
 	(*str)++;
@@ -100,6 +129,13 @@ static void	ft_match(va_list *ptr, char **str, int *counter)
 	}
 }
 
+/**
+ * @brief	Function similar to printf.
+ * 
+ * @param str Input text to write.
+ * @param ...	Variable number of input parameters.
+ * @return int	Number of characters represented.
+ */
 int	ft_printf(const char *str, ...)
 {
 	va_list	ptr;
--- circle_1/printf_pr/printf/ft_putptr_utils.c
+++ circle_1/printf_pr/printf/ft_putptr_utils.c
@@ -1,18 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putptr_fd.c                                     :+:      :+:    :+:   */
+/*   ft_putptr_utils.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jorgerod <jorgerod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 18:29:27 by jorgerod          #+#    #+#             */
-/*   Updated: 2022/10/05 15:34:30 by jorgerod         ###   ########.fr       */
+/*   Updated: 2022/10/05 16:58:50 by jorgerod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
 
+/**
+* @brief Recursive function to convert usigned decimal int to hexadecimal.
+ * 			Used with modifier 'p'.
+ * 
+ * @param i	Decimal number.
+ * @param counter Counter of characters printed by ft_printf.
+ * @param capital	If 0 number will be printed in lowercase, else in uppercase.
+ */
 static void	ft_base_change_p(unsigned long int i, int *counter, int capital)
 {
 	char			*base;
@@ -29,6 +37,14 @@ static void	ft_base_change_p(unsigned long int i, int *counter, int capital)
 	write(1, &base[i % len], 1);
 }
 
+/**
+ * @brief Recursive function to convert usigned decimal int to hexadecimal.
+ * 			Used with modifiers 'x' and 'X'.
+ * 
+ * @param i	Decimal number.
+ * @param counter Counter of characters printed by ft_printf.
+ * @param capital	If 0 number will be printed in lowercase, else in uppercase.
+ */
 static void	ft_base_change_xx(unsigned int i, int *counter, int capital)
 {
 	char			*base;
@@ -45,6 +61,14 @@ static void	ft_base_change_xx(unsigned int i, int *counter, int capital)
 	write(1, &base[i % len], 1);
 }
 
+/**
+ * @brief Represents numbers in hexadecimal for modifiers p, x and X.
+ * 
+ * @param ptr Pointer to the variable argument list.
+ * @param counter	Counter of characters printed by ft_printf.
+ * @param capital	If 0 number will be printed in lowercase, else in uppercase.
+ * @param prefix	If greater than 0, will pring '0x' before the number.
+ */
 void	ft_putptr_fd(void *ptr, int *counter, int capital, int prefix)
 {
 	unsigned long int	pp;
@@ -64,6 +88,12 @@ void	ft_putptr_fd(void *ptr, int *counter, int capital, int prefix)
 	}
 }
 
+/**
+ * @brief An extension of ft_match_case for the case of 's' specifically.
+ * 
+ * @param ptr	Pointer to variable argument list.
+ * @param counter	Counter of characters printed by ft_printf.
+ */
 void	ft_match_case_s(va_list *ptr, int *counter)
 {
 	char	*s;
@@ -81,6 +111,13 @@ void	ft_match_case_s(va_list *ptr, int *counter)
 	}
 }
 
+/**
+ * @brief Checks if is necessary to represent an space whem there are
+ * 			spaces between '%' and the conversion character.
+ * 
+ * @param str	Input string
+ * @return int	Returns 1 if an space is necessary and 0 otherwise. 
+ */
 int	ft_check_space(char *str)
 {
 	while (*str == ' ')
