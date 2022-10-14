10636fb2cc4dca749bdad632680d6c7211c22c9b
jorgerod <jorgeroma2000@gmail.com>
2022-10-14 20:08:20 +0200

    check

--- circle_1/get_next_line_pr/get_next_line/get_next_line.h
+++ circle_1/get_next_line_pr/get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jorgerod <jorgerod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 17:01:45 by jorgerod          #+#    #+#             */
-/*   Updated: 2022/10/11 18:35:14 by jorgerod         ###   ########.fr       */
+/*   Updated: 2022/10/14 20:08:03 by jorgerod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,9 @@
 
 char	*get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
+void	*ft_calloc(size_t count, size_t size);
+void	ft_bzero(void *s, size_t n);
+unsigned int	ft_strlen(const char *s);
 int ft_search (char *str, char c);
 
 #endif
